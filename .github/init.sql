f04256e414e9db4105b723c7980870cc5e8954b1
kydev <tech@knowyourself.cc>
2022-06-12 15:30:11 +0800

    Update

--- .github/init.sql
+++ .github/init.sql
@@ -2,6 +2,7 @@ CREATE TABLE `users` (
 `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
 `openid` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '员工企业微信唯一标识',
 `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '员工姓名',
+`is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除',
 `created_at` datetime NOT NULL DEFAULT '2022-01-01 00:00:00',
 `updated_at` datetime NOT NULL DEFAULT '2022-01-01 00:00:00',
 PRIMARY KEY (`id`),
